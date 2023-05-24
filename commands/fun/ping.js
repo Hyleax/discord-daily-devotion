9fb775a6ee25973d764aae758c04b78737e9ae53
Norman Yap <normanyaptw@yahoo.com>
2023-05-25 02:24:55 +1000

    discord bot working

---
+++ commands/fun/ping.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+// initialize instance of slashCommandBuilder class
+/*
+ Slash command names must be between 1-32 characters 
+ and contain no capital letters, spaces, or symbols
+*/  
+const slashCommandBuilder = new SlashCommandBuilder()
+slashCommandBuilder.setName('ping').setDescription('Replies with Pong')
+
+// provides command definition
+const execute = async(interaction) => {
+    await interaction.reply('Pong!');
+}
+
+module.exports = 
+    {
+        
+        data: slashCommandBuilder,
+        execute
+    }
