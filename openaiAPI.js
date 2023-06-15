d3c3922878920dd62cdd1649f7f3540887dd99a6
Norman Yap <normanyaptw@yahoo.com>
2023-06-15 14:56:27 +1000

    changed gpt prompt

--- openaiAPI.js
+++ openaiAPI.js
@@ -5,12 +5,13 @@ const configuration = new Configuration({
     organization: process.env.ORG_ID,
     apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 const callChatGPT = async(verse) => {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `can you give me ${verse} in NIV formatted by 'verse number. verse', can you also generate 3 devotions questions from it`,
+        prompt: `can you give me ${verse} in NIV formatted by 'verse number. verse', can you also generate 3 devotions questions from it, can you also add a fourth question analyzing this passage in detail?`,
         max_tokens: 500,
         temperature: 0,
       });
